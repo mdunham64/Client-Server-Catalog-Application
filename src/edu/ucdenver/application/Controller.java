f519c1027ffa4ca32f773638ebc84493231833e9
tzipora7 <72180653+tzipora7@users.noreply.github.com>
2020-10-26 22:30:49 -0600

    Update Controller.java

--- src/edu/ucdenver/application/Controller.java
+++ src/edu/ucdenver/application/Controller.java
@@ -1,5 +1,27 @@
 package edu.ucdenver.application;
 
+import edu.ucdenver.server.Client;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class Controller {
-    //Savannah Design and implement the UI
+    public TextField txtServerIP;
+    public TextField txtPortNum;
+    public Button btnConnectToServer;
+
+    Client client;
+    public Controller() {
+
+
+
+    }
+
+    public void ConnectToServer(ActionEvent actionEvent) {
+
+
+    }
+
+
+    
 }

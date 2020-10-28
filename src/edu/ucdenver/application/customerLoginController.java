f58eb1d9cbdf690dbdcc5b22e951381fa0ea5c6c
tzipora7 <72180653+tzipora7@users.noreply.github.com>
2020-10-28 11:56:12 -0600

    Update customerLoginController.java

--- src/edu/ucdenver/application/customerLoginController.java
+++ src/edu/ucdenver/application/customerLoginController.java
@@ -1,1 +1,21 @@
+package edu.ucdenver.application;
+import edu.ucdenver.server.Client;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
 
+
+public class customerLoginController {
+    public TextField txtServerPortErrorMessage;
+    public TextField txtUserServerIP;
+    public TextField txtUserPortNum;
+    public Button btnAttemptConnection;
+    
+    Client client;
+    
+    public void ConnectUser(ActionEvent actionEvent){
+        
+    }
+    
+    
+}

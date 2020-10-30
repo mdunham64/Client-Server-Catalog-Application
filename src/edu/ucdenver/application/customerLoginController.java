4eae3182bb3aea5b506e1803a7005bd5aed30159
MacGyver Dunham <macgyver.dunham@gmail.com>
2020-10-30 10:40:09 -0600

    Create logic to connect to the server with defined server IP and port.

--- src/edu/ucdenver/application/customerLoginController.java
+++ src/edu/ucdenver/application/customerLoginController.java
@@ -1,8 +1,12 @@
 package edu.ucdenver.application;
 import edu.ucdenver.server.Client;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 import javafx.event.ActionEvent;
+
+import java.io.IOException;
 
 
 public class customerLoginController {
@@ -19,7 +23,39 @@ public class customerLoginController {
     }
 
     public void ConnectUser(ActionEvent actionEvent){
-        
+        String serverIP = txtUserServerIP.getText();
+        String serverport = txtUserPortNum.getText();
+        int serverportint = Integer.parseInt(serverport);
+        client = new Client(serverIP,serverportint);
+        client.connect();
+        String cmd = "TEST|";
+
+        Alert alert;
+        if(client.isConnected()){
+            try {
+                String response = client.sendRequest(cmd);
+                String[] respArgs = response.split("\\|");
+
+                switch (respArgs[0]) {
+                    case "OK":
+                        alert = new Alert(Alert.AlertType.CONFIRMATION, "Action complete: " + respArgs[1], ButtonType.OK);
+                        alert.show();
+                        break;
+                    case "ERR":
+                        alert = new Alert(Alert.AlertType.ERROR, respArgs[1], ButtonType.OK);
+                        alert.show();
+                        break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                alert = new Alert(Alert.AlertType.ERROR, "Server Response:" + e.getMessage(), ButtonType.OK);
+                alert.show();
+            }
+        } else {
+            alert = new Alert(Alert.AlertType.ERROR, "Client is not connected", ButtonType.OK);
+            alert.show();
+        }
+
     }
     
     
